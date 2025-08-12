2a3c0c025858911dcd27d0a2a7f4e511cdc08563
Khanh Dang <khanh.mdang72@gmail.com>
2025-08-12 22:42:56 +0700

    fix: Minor fix

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -433,6 +433,7 @@ export const themes: ColorTheme[] = [
   {
     id: "vintage-scarlet-dark",
     name: "Vintage Scarlet",
+    isDark: true,
     colors: {
       primary: "#C1121F",
       secondary: "#FDF0D5",
